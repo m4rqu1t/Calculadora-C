b253059adbaef17ea164abb1a11ef40c56330328
m4rqu1t <marcotab422@gmail.com>
2025-08-21 10:50:46 -0300

    calculadora de operacoes basicas finalizada

--- calc.c
+++ calc.c
@@ -2,34 +2,40 @@
 int main(void){
     int oper;
     float n1, n2, soma, sub, mult, div;
-    printf("Digite:\n 1 para somar\n 2 para subtrair\n 3 para multiplicar \n 4 para dividir\n");
-    scanf("%d", &oper);
-
-    switch(oper){
-        case 1:
-            printf("Digite os numeros a serem somados separados por espaco\n");
-            scanf("%f %f", &n1, &n2);
-            soma=n1+n2;
-            printf("Resultado:\n%.2f", soma);
+    while(oper!=0){
+        printf("\nDigite:\n 1 para somar\n 2 para subtrair\n 3 para multiplicar \n 4 para dividir\n 0 para encerrar programa\n");
+        scanf("%d", &oper);
+        if(oper==0){
+            printf("Programa encerrado");
             break;
-        case 2:
-            printf("Digite os numeros a serem subtraidos separados por espaco\n");
-            scanf("%f %f", &n1, &n2);
-            sub=n1-n2;
-            printf("Resultado:\n%.2f", sub);
-            break;
-        case 3:
-            printf("Digite os numeros a serem multiplicados separados por espaco\n");
-            scanf("%f %f", &n1, &n2);
-            mult=n1*n2;
-            printf("Resultado:\n%.2f", mult);
-            break;
-        case 4:
-            printf("Digite os numeros a serem divididos separados por espaco\n");
-            scanf("%f %f", &n1, &n2);
-            div=n1/n2;
-            printf("Resultado:\n%.2f", div);
-            break;
+        }
+            switch(oper){
+                case 1:
+                    printf("Digite os numeros a serem somados separados por espaco\n");
+                    scanf("%f %f", &n1, &n2);
+                    soma=n1+n2;
+                    printf("Resultado:\n%.2f", soma);
+                    break;
+                case 2:
+                    printf("Digite os numeros a serem subtraidos separados por espaco\n");
+                    scanf("%f %f", &n1, &n2);
+                    sub=n1-n2;
+                    printf("Resultado:\n%.2f", sub);
+                    break;
+                case 3:
+                    printf("Digite os numeros a serem multiplicados separados por espaco\n");
+                    scanf("%f %f", &n1, &n2);
+                    mult=n1*n2;
+                    printf("Resultado:\n%.2f", mult);
+                    break;
+                case 4:
+                    printf("Digite os numeros a serem divididos separados por espaco\n");
+                    scanf("%f %f", &n1, &n2);
+                    div=n1/n2;
+                    printf("Resultado:\n%.2f", div);
+                    break;
+                
+            }
     }
     return 0;
 }
